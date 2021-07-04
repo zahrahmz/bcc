7544ba6e9382c1eb9a4f9ffd0c27ce9a72a8f99e
Zahra <z.hamzeh@irantalent.com>
2021-07-04 23:46:00 +0430

    certificates.

--- app/Http/Controllers/ZahraController.php
+++ app/Http/Controllers/ZahraController.php
@@ -10,6 +10,6 @@ class ZahraController extends BaseController
     public function index()
     {
         $this->setPageTitle('فروشگاه اینترنتی بی سی سی');
-        return view('site.ZAHRA.stores');
+        return view('site.ZAHRA.certificates');
     }
 }
---
+++ resources/views/site/ZAHRA/certificates.blade.php
@@ -0,0 +1,33 @@
+@extends('site.app')
+@section('title') {{ $pageTitle }} @endsection
+@section('content')
+    <div class="page-content">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h1 class="page-title text-center">
+                    گواهینامه ها
+                </h1>
+            </div>
+            <div class="col-12 col-md-8">
+                <div class="row justify-content-between">
+                    <div class="col-12 col-sm-6 col-md-3 p-5 d-flex flex-column align-items-center">
+                        <img class="w-100 h-auto border" src="https://bccstyle.com/uploads/21546948495.jpg" alt="">
+                        <p class="mt-3">گواهینامه</p>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-3 p-5 d-flex flex-column align-items-center">
+                        <img class="w-100 h-auto border" src="https://bccstyle.com/uploads/21546948495.jpg" alt="">
+                        <p class="mt-3">گواهینامه</p>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-3 p-5 d-flex flex-column align-items-center">
+                        <img class="w-100 h-auto border" src="https://bccstyle.com/uploads/21546948495.jpg" alt="">
+                        <p class="mt-3">گواهینامه</p>
+                    </div>
+                    <div class="col-12 col-sm-6 col-md-3 p-5 d-flex flex-column align-items-center">
+                        <img class="w-100 h-auto border" src="https://bccstyle.com/uploads/21546948495.jpg" alt="">
+                        <p class="mt-3">گواهینامه</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
